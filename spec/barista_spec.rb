156582b6de06c52925a8c2542b5057e462ede8c2
dblock <dblock@dblock.org>
2011-08-18 13:49:08 -0400

    Wrote a couple of simple specs.

--- spec/barista_spec.rb
+++ spec/barista_spec.rb
@@ -6,10 +6,25 @@ describe Barista do
 
   context 'configuration'
 
-  context 'setting the root'
+  context 'setting app_root' do
+    it "defaults to Rails.root" do
+      Barista::app_root.should == Rails.root
+    end
+    it "can be set to another directory" do
+      new_path = File.expand_path("../../public/javascripts", __FILE__)
+      Barista.configure do |c|
+        c.app_root = new_path
+      end
+      Barista::app_root.to_s.should == new_path
+    end
+  end
 
   context 'compiling files'
 
-  context 'compiling all'
+  context 'compiling all' do
+    it "compiles nothing" do
+      lambda { Barista::compile_all! false, false }.should_not raise_error
+    end
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 
 ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ROOT'] ||= File.expand_path("..", __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
@@ -9,6 +10,8 @@ require "active_resource/railtie"
 require 'rspec/rails'
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+class Application < Rails::Application; end
 
 require 'barista'
 
